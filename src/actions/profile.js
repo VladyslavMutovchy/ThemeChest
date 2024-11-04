3c39a5459b61d8470fae8bc493fd0f007fc2b762
Vladyslav Mutovchy <fogekko@gmail.com>
2024-11-04 17:22:27 +0100

    Bearer token

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,11 +1,11 @@
 import { profileAPI } from '../api/profile.js';
 import { actionWrapper } from './actionWrapper';
-import { isFetching } from './preloader'; 
+import { isFetching } from './preloader';
 
 export const SET_USER_DATA = 'SET_USER_DATA';
 export const SET_FORM_ERROR = 'SET_FORM_ERROR';
-export const SAVE_DATA = 'SAVE_DATA';  
-export const GET_DATA = 'GET_DATA'; 
+export const SAVE_DATA = 'SAVE_DATA';
+export const GET_DATA = 'GET_DATA';
 
 export const SET_ERROR = 'SET_ERROR';
 
@@ -14,48 +14,49 @@ export const setError = (hasError) => ({
   payload: hasError,
 });
 
+export const updateUserProfile = (userData, callback, errorCallback) =>
+  actionWrapper(async (dispatch) => {
+    dispatch(isFetching(true));
+    try {
+      const newUserData = await profileAPI.updateProfile(userData);
+      dispatch(setUserData(newUserData)); 
+      callback?.(newUserData); 
+    } catch (error) {
+      errorCallback?.(error);
+    } finally {
+      dispatch(isFetching(false));
+    }
+  });
 
-export const updateUserProfile = (userData, callback, errorCallback) => actionWrapper(async (dispatch) => {
-  dispatch(isFetching(true));
-  try {
-    const newUserData = await profileAPI.updateProfile(userData);
-    dispatch(setUserData(newUserData)); // Обновляем стор
-    callback?.(newUserData); // Передаём обновлённые данные в callback
-  } catch (error) {
-    errorCallback?.(error);
-  } finally {
-    dispatch(isFetching(false));
-  }
-});
+export const changePasswordAction = (id, values, callback, errorCallback) =>
+  actionWrapper(async (dispatch) => {
+    dispatch(isFetching(true));
+    try {
+      await profileAPI.updatePassword(id, values);
+      callback?.();
+    } catch (error) {
+      errorCallback?.(error);
+    } finally {
+      dispatch(isFetching(false));
+    }
+  });
 
-
-
-export const changePasswordAction = (id, values, callback, errorCallback) => actionWrapper(async (dispatch) => {
-  dispatch(isFetching(true)); 
-  try {
-    await profileAPI.updatePassword(id, values);
-    callback?.();
-  } catch (error) {
-    errorCallback?.(error);
-  } finally {
-    dispatch(isFetching(false));  
-  }
-});
-
-export const getUserData = (id, callback, errorCallback) => actionWrapper(async (dispatch) => {
-  dispatch(isFetching(true));  
-  try {
-    const userData = await profileAPI.fetchProfile(id);
-    callback?.();
-    dispatch(setUserData(userData)); 
-  } catch (error) {
-    errorCallback?.(error);
-  } finally {
-    dispatch(isFetching(false));  
-  }
-});
-
-
+export const getUserData = (id, callback, errorCallback) =>
+  actionWrapper(async (dispatch) => {
+    dispatch(isFetching(true));
+    try {
+      const userData = await profileAPI.fetchProfile(id);
+      if (userData.photo && userData.mimeType) {
+        userData.photoUrl = `data:${userData.mimeType};base64,${userData.photo}`; 
+      }
+      dispatch(setUserData(userData));
+      callback?.();
+    } catch (error) {
+      errorCallback?.(error);
+    } finally {
+      dispatch(isFetching(false));
+    }
+  });
 
 export const setUserData = (profile) => {
   return {
--- src/api/api.js
+++ src/api/api.js
@@ -1,50 +1,33 @@
 import axios from 'axios';
-
 import { backURL } from '../config';
 
-const authHeader = (json = false) => {
-  const contentType = json ? 'application/json' : 'application/x-www-form-urlencoded';
-  const headers = { 'content-type': contentType };
-
-  const user = JSON.parse(localStorage.getItem('user'));
-
-  if (user?.token) {
-    headers['x-solt'] = user.token;
-  }
-
-  return headers;
-};
-
-const addInterceptors = (instance, json) => {
-  instance.interceptors.request.use((config) => {
-    if (!config.headers['x-solt']) {
-      config.headers = authHeader(json);
-    }
-    return config;
-  }, Promise.reject);
+const authHeader = () => {
+  const token = JSON.parse(localStorage.getItem('user'))?.token;
+  return token ? { Authorization: `Bearer ${token}` } : {};
 };
 
 const axiosInstance = axios.create({
-  async: true,
-  crossDomain: true,
   baseURL: backURL,
-  headers: authHeader(true),
+  crossDomain: true,
 });
 
-addInterceptors(axiosInstance, true);
+axiosInstance.interceptors.request.use((config) => {
+  config.headers = { ...config.headers, ...authHeader() }; // Добавляем заголовок с токеном
+  return config;
+}, (error) => Promise.reject(error));
 
+// Создаем экземпляр axios для работы с файлами
 const axiosFileInstance = axios.create({
-  async: true,
-  crossDomain: true,
   baseURL: backURL,
-  headers: authHeader(),
-  processData: false,
-  contentType: false,
-  mimeType: 'multipart/form-data',
+  crossDomain: true,
 });
 
-addInterceptors(axiosFileInstance);
+axiosFileInstance.interceptors.request.use((config) => {
+  config.headers = { ...config.headers, ...authHeader() }; // Добавляем заголовок с токеном для файлов
+  return config;
+}, (error) => Promise.reject(error));
 
+// Определяем методы для запросов
 const post = async (url, data, callback, errorCallback) => {
   const response = await axiosInstance.post(url, JSON.stringify(data)).then(callback).catch(errorCallback);
   return response.data;
@@ -61,7 +44,7 @@ const deleteById = async (url, ...args) => {
   const id = args[index++];
   const callback = args[index++];
   const errorCallback = args[index];
-  
+
   const response = await axiosInstance.delete(`${url}/${id}`).then(callback).catch(errorCallback);
   return response.data;
 };
@@ -69,11 +52,10 @@ const deleteById = async (url, ...args) => {
 const get = async (url, ...args) => {
   let index = typeof args[0] === 'function' ? -1 : 0;
 
-  const data = args[index++];
   const callback = args[index++];
   const errorCallback = args[index];
 
-  const response = await axiosInstance.get(url, data).then(callback).catch(errorCallback);
+  const response = await axiosInstance.get(url).then(callback).catch(errorCallback);
   return response.data;
 };
 
